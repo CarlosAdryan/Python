a5f954442a812b1aad87ce20c471ddef89b4b7a3
Carlos Adryan <carlosadryana@gmail.com>
2025-05-09 13:40:50 -0300

    Variable declaration and assignment

---
+++ variables-and-data/program.py
@@ -0,0 +1,17 @@
+age: int
+salary: float
+height: float
+gender: str
+name: str
+
+age = 20
+salary = 5800.5
+height = 1.63
+gender = "F"
+name = "Maria Silva"
+
+print(f"AGE = {age}")
+print(f"SALARY = {salary:.2f}")
+print(f"HEIGHT = {height:.2f}")
+print(f"GENDER = {gender}")
+print(f"NAME = {name}")
