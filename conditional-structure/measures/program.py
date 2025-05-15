848fedad8162f294933204acaa916c353cfbd3f3
Carlos Adryan <carlosadryana@gmail.com>
2025-05-15 11:42:41 -0300

    Calculation of geometric areas based on measurements A, B and C

---
+++ conditional-structure/measures/program.py
@@ -0,0 +1,12 @@
+a = float(input("Enter measure A: "))
+b = float(input("Enter measure B: "))
+c = float(input("Enter measure C: "))
+
+squareArea = a * a
+triangleArea = (a * b) / 2
+trapezioArea = ((a + b) * c) / 2
+
+print(f"SQUARE AREA = {squareArea:.4f}")
+print(f"TRIANGLE AREA = {triangleArea:.4f}")
+print(f"TRAPEZIO AREA = {trapezioArea:.4f}")
+
