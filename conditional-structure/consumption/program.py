431aa7f8d8ae81a245a197a72703a6b662dc7b59
Carlos Adryan <carlosadryana@gmail.com>
2025-05-15 09:11:12 -0300

    Average consumption problem with formatted output

---
+++ conditional-structure/consumption/program.py
@@ -0,0 +1,6 @@
+distanceTraveled = int(input("Distance traveled: "))
+fuelSpent = float(input("Fuel spent: "))
+
+averageConsumption = distanceTraveled / fuelSpent
+
+print(f"Average consumption: {averageConsumption:.3f}")
