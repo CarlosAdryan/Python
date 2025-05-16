e81683ee9c7d9cfc2a611c83378539bd01af345a
Carlos Adryan <carlosadryana@gmail.com>
2025-05-16 08:33:54 -0300

    Calculation of the final grade with disapproval rule

---
+++ conditional-structure/notes/program.py
@@ -0,0 +1,8 @@
+note1 = float(input("Enter the first note: "))
+note2 = float(input("Type the second note: "))
+
+finalNote = note1 + note2
+
+print(f"FINAL NOTE = {finalNote}")
+if finalNote < 60.00:
+    print("REPROVED")
