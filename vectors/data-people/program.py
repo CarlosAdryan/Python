a768e31937c9fe70bc67fe01356d4de4761e4a66
Carlos Adryan <carlosadryana@gmail.com>
2025-05-27 20:16:08 -0300

    Input flow and statistical calculation

---
+++ vectors/data-people/program.py
@@ -0,0 +1,34 @@
+n = int(input("How many people will be typed? "))
+
+height: [float] = [0 for x in range(n)]
+genre: [str] = [0 for x in range(n)]
+
+minor = 0
+bigger = 0
+averageWomen = 0
+womenSum = 0
+typedWomen = 0
+typedMen = 0
+
+for i in range(0, n):
+    height[i] = float(input(f"Height of the {i}st person: "))
+    genre[i] = (input(f"Genre of the {i}st person: "))
+    minor = height[0]
+    if height[i] > bigger:
+        bigger = height[i]
+    if genre[i] == "f":
+        typedWomen += 1
+        womenSum += height[i]
+    elif genre[i] == "m":
+        typedMen += 1
+
+for i in range(0, n):
+    if height[i] < minor:
+        minor = height[i]
+
+averageWomen = womenSum / typedWomen
+print(f"Lower height = {minor}")
+print(f"Higher height = {bigger}")
+print(f"Women's heights average = {averageWomen:.2f}")
+print(f"Number of men = {typedMen}")
+
